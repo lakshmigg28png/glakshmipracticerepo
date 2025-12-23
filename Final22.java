722f10c9be5599d329342d49140cd136a8df21ee
lakshmigg28png <lakshmigg28@gmail.com>
2025-12-23 10:20:30 +0530

    Add files via upload

---
+++ Final22.java
@@ -0,0 +1,10 @@
+package oopsAssignments;
+class GovernmentRules {
+    final int MAX_WORKING_HOURS = 8;
+public class Final22 {
+	 GovernmentRules rules = new GovernmentRules();
+    
+     System.out.println("Maximum working hours: " + rules.MAX_WORKING_HOURS);
+ }
+
+}
