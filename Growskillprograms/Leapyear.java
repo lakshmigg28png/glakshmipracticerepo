62d99c72d96ac6ba1409566b7580e5b1bf9b61b6
lakshmigg28png <lakshmigg28@gmail.com>
2026-01-05 08:00:35 +0530

    Add files via upload

---
+++ Growskillprograms/Leapyear.java
@@ -0,0 +1,16 @@
+package assignmentsgrowskill;
+
+public class Leapyear {
+
+	public static void main(String[] args) {
+		int year=2025;
+		if(year%4==0) {
+			System.out.println(year+" is a leap year.");
+		}else {
+				System.out.println(year+" is not a leap year.");
+			}
+		}
+
+	}
+
+
