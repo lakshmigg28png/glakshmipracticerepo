3d3015f9c4c542c3fc302627a53024d1207d14a6
lakshmigg28png <lakshmigg28@gmail.com>
2026-01-05 08:03:06 +0530

    Add files via upload

---
+++ Growskillprograms/NumberCheck4.java
@@ -0,0 +1,16 @@
+package assignmentsgrowskill;
+
+public class NumberCheck4 {
+
+	public static void main(String[] args) {
+		 int num = 56; 
+	        if ((num > 0 && num % 4 == 0) || (num < 0 && num % 6 == 0)) {
+	            System.out.println("Yes");
+	        } else {
+	            System.out.println("No");
+	        }
+	    }
+
+	}
+
+
