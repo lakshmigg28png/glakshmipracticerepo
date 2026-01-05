e2a24da094fe3a5e45c0e931b4d63a9ac5a13e78
lakshmigg28png <lakshmigg28@gmail.com>
2026-01-05 07:59:45 +0530

    Add files via upload

---
+++ Growskillprograms/DoWhile.java
@@ -0,0 +1,14 @@
+package assignmentsgrowskill;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		  boolean condition = false;
+	        do {
+	            System.out.println("Loop Executed");
+	        } while (condition);
+	    }
+
+	}
+
+
